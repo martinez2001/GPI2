96a31fffe7d81c4b934b878941f941c97dd51640
martinez2001 <99246871+martinez2001@users.noreply.github.com>
2023-05-22 17:51:37 +0200

    Actualizar bash de automatizacion tras tutoría

--- proyectoNFC/automatizacion.sh
+++ proyectoNFC/automatizacion.sh
@@ -4,16 +4,22 @@
 git clone https://github.com/ikarus23/MifareClassicTool.git proyectoNFC
 cd proyectoNFC/
 
-# Compilar utilizando ./gradlew
-cd Mifare\ Classic\ Tool/
-./gradlew
-
 # Ejecutar el comando PMD
 /home/alumno/Descargas/pmd-bin-7.0.0-rc2/bin/pmd check -d "/home/alumno/android/MifareClassicTool-master/Mifare Classic Tool/" --rulesets=/home/alumno/android/custom-ruleset.xml -r /home/alumno/android/resultadosPMD.txt
 
+# Compilar utilizando ./gradlew
+cd Mifare\ Classic\ Tool/
+export ANDROID_HOME=/home/alumno/android/
+export ANDROID_SDK_ROOT=/home/alumno/android/
+./gradlew :app:build
+
+
+
 #--------------------------------
 #Easybuggy
-cd
-git clone https://github.com/k-tamura/easybuggy.git easyBuggy
-cd easyBuggy/
-mvn clean install
+#cd
+#git clone https://github.com/k-tamura/easybuggy.git easyBuggy
+#cd easyBuggy/
+# Ejecutar el comando PMD
+#/home/alumno/Descargas/pmd-bin-7.0.0-rc2/bin/pmd check -d "/home/alumno/easyBuggy/" --rulesets=/home/alumno/android/custom-ruleset.xml -r /home/alumno/android/resultadosPMDeasyBuggy.txt
+#mvn clean install
